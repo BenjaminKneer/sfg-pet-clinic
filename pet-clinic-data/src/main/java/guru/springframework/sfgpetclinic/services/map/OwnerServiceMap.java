98ca93b0271180b758df117d628aa26e75d35bea
bkneer <benjamin.kneer@geneon.de>
2021-05-29 16:26:21 +0200

    * DataLoader - loading Owners & Vets

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,7 +1,12 @@
 package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.services.OwnerService;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> {
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService{
 
+    @Override
+    public Owner findByLastName(String name) {
+        return null;
+    }
 }
