0865b0d6d23719d3b11d8d3689905ee3c8e8189c
bkneer <benjamin.kneer@geneon.de>
2021-05-30 18:03:55 +0200

    * PetType Bootstrap

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,38 +1,49 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+
+    public DataLoader(OwnerService ownerService,
+                      VetService vetService,
+                      PetTypeService petTypeService) {
+
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
-
 
     @Override
     public void run(String... args) throws Exception {
-        Owner owner1 = new Owner();
-        owner1.setFirstName("michael");
-        owner1.setLastName("weston");
+        List<PetType> petTypeList = createPetTypeData();
+        System.out.println("loaded pettypes");
 
-        Owner owner2 = new Owner();
-        owner2.setFirstName("blub");
-        owner2.setLastName("keks");
-
-        ownerService.save(owner1);
-        ownerService.save(owner2);
-
+        List<Owner> ownerList = createOwnerData();
         System.out.println("loaded owners");
+
+        List<Vet> vetList = createVetData();
+        System.out.println("loaded vets");
+
+    }
+
+    private List<Vet> createVetData() {
+        List<Vet> vetList = new ArrayList<>();
 
         Vet vet1 = new Vet();
         vet1.setFirstName("firstnmame bla");
@@ -42,10 +53,41 @@ public class DataLoader implements CommandLineRunner {
         vet2.setFirstName("firstnmame blub");
         vet2.setLastName("lastname blub");
 
-        vetService.save(vet1);
-        vetService.save(vet2);
+        vetList.add(vetService.save(vet1));
+        vetList.add(vetService.save(vet2));
 
-        System.out.println("loaded vets");
+        return vetList;
+    }
 
+    private List<Owner> createOwnerData() {
+        List<Owner> ownerList = new ArrayList<>();
+
+        Owner owner1 = new Owner();
+        owner1.setFirstName("michael");
+        owner1.setLastName("weston");
+
+        Owner owner2 = new Owner();
+        owner2.setFirstName("blub");
+        owner2.setLastName("keks");
+
+        ownerList.add(ownerService.save(owner1));
+        ownerList.add(ownerService.save(owner2));
+
+        return ownerList;
+    }
+
+    private List<PetType> createPetTypeData() {
+        List<PetType> petTypeList = new ArrayList<>();
+
+        PetType petType1 = new PetType();
+        petType1.setName("dog");
+
+        PetType petType2 = new PetType();
+        petType2.setName("cat");
+
+        petTypeList.add(petTypeService.save(petType1));
+        petTypeList.add(petTypeService.save(petType2));
+
+        return petTypeList;
     }
 }
