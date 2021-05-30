2e673f6799276822c6544d2562eab150c9bf5426
bkneer <benjamin.kneer@geneon.de>
2021-05-30 17:43:37 +0200

    * Owner Details

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,4 +1,44 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
+
+    private String address;
+    private String city;
+    private String telephon;
+    private Set<Pet> pets;
+
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTelephon() {
+        return telephon;
+    }
+
+    public void setTelephon(String telephon) {
+        this.telephon = telephon;
+    }
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
