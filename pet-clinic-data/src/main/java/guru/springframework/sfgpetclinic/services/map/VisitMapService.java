313b0131f5703d4c5f1211e68b87e26b83377b72
bkneer <benjamin.kneer@geneon.de>
2021-06-14 09:37:45 +0200

    * VisitSDJpaService

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -2,19 +2,15 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Visit;
 import guru.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService{
-
-    private final VisitService visitService;
-
-    public VisitMapService(VisitService visitService) {
-        this.visitService = visitService;
-    }
 
     @Override
     public Visit save(Visit visit) {
 
-        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null) {
             throw new RuntimeException("invalid visit");
         }
 
